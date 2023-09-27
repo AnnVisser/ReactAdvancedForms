d062c1fcdd010dde9493324529db3fb485eb9533
Annemarie Visser <annvisser@ziggo.nl>
2023-09-27 12:26:09 +0200

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,50 @@
+import { useState } from "react";
+
 function App() {
-  return (
-    <div className="App">
-      <h1>Our beautiful form will be here:</h1>
-      <h1>Quotes:</h1>
-      <div className="quote">
-        <p>
-          My biggest fear is that people will attribute fake quotes to me and
-          millions of morons on the internet will believe it.
-        </p>
-        - <span>Albert Einstein</span>
-      </div>
-    </div>
-  );
+	const [quotes, setQuotes] = useState([
+		{
+			quote:
+				"My biggest fear is that people will attribute fake quotes to me and millions of morons on the internet will believe it.",
+			name: "Albert Einstein",
+		},
+	]);
+	const [quote, setQuote] = useState("");
+	const [name, setName] = useState("");
+
+	const addQuote = (event) => {
+		event.preventDefault();
+		setQuotes((quotes) => [{ quote, name }, ...quotes]);
+		setQuote("");
+		setName("");
+	};
+
+	return (
+		<div className='App'>
+			<h1>Our beautiful form will be here:</h1>
+			<form onSubmit={addQuote}>
+				<textarea
+					onChange={(e) => setQuote(e.target.value)}
+					value={quote}
+					rows={4}
+				/>
+				<input
+					placeholder='name'
+					onChange={(e) => setName(e.target.value)}
+					value={name}
+				/>
+				<button type='submit'>Add Quote</button>
+			</form>
+			<h1>Quotes:</h1>
+			{quotes.map((quote) => {
+				return (
+					<div className='quote' key={quote.quote}>
+						<p>{quote.quote}</p>- <span>{quote.name}</span>
+						<hr />
+					</div>
+				);
+			})}
+		</div>
+	);
 }
 
 export default App;
